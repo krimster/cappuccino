be385c5ba53a60bd48e556fad9ecd0665a9e9f5a
krime <erickrim@yahoo.co.uk>
2017-01-16 10:48:14 +0000

    Added a payload page

---
+++ src/main/java/com/cappuccino/backend/persistence/domain/backend/User.java
@@ -0,0 +1,7 @@
+package com.cappuccino.backend.persistence.domain.backend;
+
+/**
+ * Created by krime on 1/16/17.
+ */
+public class User {
+}
---
+++ src/main/java/com/cappuccino/web/controllers/PayloadController.java
@@ -0,0 +1,20 @@
+package com.cappuccino.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by krime on 1/16/17.
+ */
+@Controller
+public class PayloadController {
+
+    public static final String PAYLOAD_VIEW_NAME = "payload/payload";
+
+    @RequestMapping("/payload")
+    public String payload() {
+        return PAYLOAD_VIEW_NAME;
+    }
+}
+
+
