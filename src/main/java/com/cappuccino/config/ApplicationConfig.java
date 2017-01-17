0227d9b4f1f6224955737f532bc8b0d22011c10a
krime <erickrim@yahoo.co.uk>
2017-01-17 10:43:44 +0000

    Added admin user

--- src/main/java/com/cappuccino/config/ApplicationConfig.java
+++ src/main/java/com/cappuccino/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.cappuccino.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,5 +13,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.cappuccino.backend.persistence.repositories")
 @EntityScan(basePackages = "com.cappuccino.backend.persistence.domain.backend")
 @EnableTransactionManagement
+@PropertySource("file:///${user.home}/.cappuccino/application-common.properties")
 public class ApplicationConfig {
 }
